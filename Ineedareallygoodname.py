714c78cca0245f7e9046e9add759aeabdbe91ca5
EgoAlleyTriple <noahbozzu@gmail.com>
2025-07-25 14:07:04 +0000

    Working on recursion again and again

---
+++ Ineedareallygoodname.py
@@ -0,0 +1,13 @@
+# def cool(x):
+#     print("cool", x)
+#     if x>500:
+#         cool(x-70)
+#     else:
+#         cool(x-50)
+# cool(2270)
+x=1
+def recursivefactorial(x):
+    print("")
+    if 
+    return recursivefactorial(x-1)*x
+print(recursivefactorial(5))
